e3032bc2dd98624f8fc72e68ae12e2109dd3c7b1
serhii <popadukserhiy@gmail.com>
2026-06-11 09:41:42 +0300

    chore: allow LAN dev origin for phone testing

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  allowedDevOrigins: ['192.168.88.46'],
 };
 
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
